2f19e6535d504bb6fdc792ec3d2fc485529926f8
townesholer <townesholer@gmail.com>
2026-05-05 17:08:08 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -2,10 +2,10 @@ const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
-    'Hi，我是艺术爱好者',
-    'Hi，我是教育从业者',
-    'Hi，我是人类先行者',
-    '欢迎来到我的艺术空间🎉'
+    'Hi，我是一个小画家',
+    'Hi，我是一个小老师',
+    'Hi，我是一个干饭人',
+    '欢迎来到我的画廊🎉'
   ], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
